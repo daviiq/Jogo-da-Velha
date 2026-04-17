fcf04e4af649f2181df6c6501bac54485f73f779
Davi Israel Quirino <da059230@intelbras.com.br>
2026-04-17 14:13:27 -0300

    Atualização no Jogo da Velha

--- Jogo.py
+++ Jogo.py
@@ -4,3 +4,31 @@ import Tabuleiro
 
 #Chama o tabuleiro
 Tabuleiro.mostrar_tabuleiro()
+
+#Define que o jogador atual é o jogador 1
+jogador_atual = Jogador.jogador1
+
+#Loop para jogar
+while True:
+    print(f"Jogador {jogador_atual.simbolo}")
+    linha = int(input("Linha (0-2): "))
+    coluna = int(input("Coluna (0-2): "))
+
+    if Tabuleiro.jogar(linha,coluna,jogador_atual.simbolo):
+        Tabuleiro.mostrar_tabuleiro()
+
+        #Verifica a Vitória
+        if Tabuleiro.verificar_vitoria(jogador_atual.simbolo):
+            print(f"Jogador {jogador_atual.simbolo} venceu !")
+            break
+
+        #Verifica Empate
+        if Tabuleiro.verificar_empate():
+            print("Empate!")
+            break
+
+        #Alterna entre jogadores
+        if jogador_atual == Jogador.jogador1:
+            jogador_atual = Jogador.jogador2
+        else:
+            jogador_atual = Jogador.jogador1
--- Tabuleiro.py
+++ Tabuleiro.py
@@ -9,3 +9,26 @@ def mostrar_tabuleiro():
     for linha in tabuleiro:
         print(" | ".join(linha))
         print("-" * 9)
+
+#Cria o método para jogar
+def jogar(linha, coluna, simbolo):
+    if tabuleiro[linha][coluna] == " ":
+        tabuleiro[linha][coluna] = simbolo
+        return True
+    else:
+        print("Posição já ocupada!")
+        return False
+
+#Verificar a vitória
+def verificar_vitoria(simbolo):
+    #verifica as linhas
+    for i in range(3):
+        if tabuleiro[i][0] == simbolo and tabuleiro[i][1] == simbolo and tabuleiro[i][2] == simbolo:
+            return True
+        
+    #verifica as colunas
+    for j in range(3):
+        if tabuleiro[0][j] == simbolo and tabuleiro[1][j] == simbolo and tabuleiro [2][j] == simbolo: return True
+
+    #Verificar Diagonal principal
+    if tabuleiro[0][0] == simbolo and tabuleiro[1][1] == simbolo and tabuleiro[2][2] == simbolo: return True
